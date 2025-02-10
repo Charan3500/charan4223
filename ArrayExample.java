1f64156374466bc599ac4743c5911d0dbe6340be
Charan3500 <saicharansjp413@gmail.com>
2025-02-10 15:42:48 +0530

    arrayexample

---
+++ ArrayExample.java
@@ -0,0 +1,14 @@
+public class ArrayExample2 {
+	
+	public static void main(String[] args) {
+		char[] age={65,66,67,68,69};
+		
+		System.out.println(age[0]);
+		System.out.println(age[1]);
+		System.out.println(age[2]);
+		System.out.println(age[3]);
+		System.out.println(age[4]);
+	
+	}
+
+} 
